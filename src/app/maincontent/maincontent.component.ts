02f0efeddf735466cd4d619cd5b6bc4b1484ae50
Christian Duus <duus92@web.de>
2025-03-21 13:46:59 +0100

    About me Constructed

--- src/app/maincontent/maincontent.component.ts
+++ src/app/maincontent/maincontent.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { FeedbacksComponent } from '../feedbacks/feedbacks.component';
 import { ContactComponent } from '../contact/contact.component';
+import { SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-maincontent',
   standalone: true,
-  imports: [AboutmeComponent, FeedbacksComponent, ContactComponent],
+  imports: [AboutmeComponent, FeedbacksComponent, ContactComponent, SkillsComponent],
   templateUrl: './maincontent.component.html',
   styleUrl: './maincontent.component.scss'
 })
