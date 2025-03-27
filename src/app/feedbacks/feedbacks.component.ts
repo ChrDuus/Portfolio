55a6365be130adbb2bbc71cfc2e6fc01f2b89ebe
ChrDuus <duus92@web.de>
2025-03-27 17:06:36 +0100

    Feedback Section finished

--- src/app/feedbacks/feedbacks.component.ts
+++ src/app/feedbacks/feedbacks.component.ts
@@ -14,6 +14,69 @@ feedbacks :{name: string; feedback: string, ref: string}[]=
   name: 'test',
   feedback: 'doppelter test',
   ref: 'Boss'
+},
+{
+  name: 'anderer test',
+  feedback: 'hier wird auch getestet',
+  ref: 'employee'
+},
+{
+  name: 'mustermann, max',
+  feedback: 'ewige testung',
+  ref: 'putzfrau'
+},
+{ feedback: "Great teamwork!",
+   name: "Alice",
+    ref: "Software Engineer"
+   },
+{ feedback: "Very reliable.",
+   name: "Bob",
+    ref: "Project Manager"
+   },
+  
+];
+currentOffset = 0;
+
+middleIndex = 2; 
+
+
+slideLeft() {
+  if(this.middleIndex < this.feedbacks.length -1){
+    this.middleIndex++;
+  }else{this. middleIndex = this.middleIndex}  
+  this.shiftCards('right');
+  
 }
-]
+
+slideRight() {
+  if (this.middleIndex != 0) {
+    this.middleIndex--; 
+  }else{
+    this.middleIndex = 0
+  }
+  this.shiftCards('left');
+ 
+}
+
+shiftCards(direction: string) {
+  const feedbackCards = document.querySelectorAll('.feedback-card');
+  if (direction === 'left') {
+    this.currentOffset += 105; 
+  } else if (direction === 'right') {
+    this.currentOffset -= 105; 
+  }
+  if(this.currentOffset > 210){
+    this.currentOffset = 210;
+  }if(this.currentOffset < -210){
+    this.currentOffset= -210
+  }
+  feedbackCards.forEach((card: any, index:number) => {
+    const isActive = index === this.middleIndex;
+    card.style.transform = `translateX(${this.currentOffset}%) scale(${isActive ? 1.1 : 0.8})` ;
+  });
+}
+
+
+
+
 }
