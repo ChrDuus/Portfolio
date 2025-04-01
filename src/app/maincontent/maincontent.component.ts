a92050d249b102122a172fe23227b987c2de4ac6
ChrDuus <duus92@web.de>
2025-04-01 16:47:44 +0200

    Impressum added

--- src/app/maincontent/maincontent.component.ts
+++ src/app/maincontent/maincontent.component.ts
@@ -4,11 +4,12 @@ import { FeedbacksComponent } from '../feedbacks/feedbacks.component';
 import { ContactComponent } from '../contact/contact.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { LandingpageComponent } from '../landingpage/landingpage.component';
 
 @Component({
   selector: 'app-maincontent',
   standalone: true,
-  imports: [AboutmeComponent, FeedbacksComponent, ContactComponent, SkillsComponent, PortfolioComponent],
+  imports: [AboutmeComponent, FeedbacksComponent, ContactComponent, SkillsComponent, PortfolioComponent, LandingpageComponent],
   templateUrl: './maincontent.component.html',
   styleUrl: './maincontent.component.scss'
 })
