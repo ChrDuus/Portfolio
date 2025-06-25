bb9438dd444735599dd9f6106a8abefbb062bd2b
ChrDuus <duus92@web.de>
2025-06-25 11:43:36 +0200

    burger menu closes on click outside now

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -13,9 +13,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private renderer: Renderer2, private translationService: TranslationService) {
+  constructor(private renderer: Renderer2, private translationService: TranslationService, private elementRef: ElementRef) {
     this.screenWidth = window.innerWidth;
-    this.activeLanguage = this.translationService.getCurrentLanguage(); // Stelle sicher, dass die aktuelle Sprache aus dem TranslationService kommt
+    this.activeLanguage = this.translationService.getCurrentLanguage(); 
   }
 
   screenWidth: number;
@@ -26,6 +26,7 @@ export class HeaderComponent {
   @ViewChild('langBtnEn') langBtnEn!: ElementRef<HTMLParagraphElement>;
   @ViewChild('langBtnDeMobile') langBtnDeMobile!: ElementRef<HTMLParagraphElement>;
   @ViewChild('langBtnEnMobile') langBtnEnMobile!: ElementRef<HTMLParagraphElement>;
+  @ViewChild('overlay') overlayRef!: ElementRef;
 
   toggleLanguageColor() {
     this.langBtnDe.nativeElement.classList.toggle('active');
@@ -51,5 +52,16 @@ export class HeaderComponent {
   onResize(event:any) {
     this.screenWidth = window.innerWidth;
   }
+
+ @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (clickedInside && this.mobileMenuActive) {
+      setTimeout(() => {
+        this.mobileMenuActive = false;
+      }, 500);      
+    } 
+}
+
 }
 
